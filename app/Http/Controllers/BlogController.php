943e70c51f2dfea59e18bab0f8907a0cedb9f3bf
Fernando Valencia <fvalenciaf@gmail.com>
2017-10-19 03:28:56 -0300

    Add search bar functionality.

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,43 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use App\Post;
 use App\Category;
-
+use App\Post;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     //
-	public function getIndex(){
+    public function getIndex(Request $request)
+    {
+        $posts = $request->q ?
+        Post::searchTitle($request->q)->orderBy('id', 'desc')->paginate(10) :
+        Post::where('published', '=', 1)->orderBy('id', 'desc')->paginate(10);
 
-		$posts=Post::where('published','=',1)->orderBy('id','desc')->paginate(10);
+        $catcnt = [];
+        $posts_to_cnt = Post::where('published', 1)->get();
+        foreach ($posts_to_cnt as $post) {
+            $catcnt[$post->category_id] = Post::where('category_id', $post->category_id)->count();
+        }
+        $categories = Category::all();
 
-		$catcnt=[];
-		$posts_to_cnt=Post::where('published',1)->get();
-		foreach ($posts_to_cnt as $post) 
-		{
-			$catcnt[$post->category_id]=Post::where('category_id',$post->category_id)->count();	
-		}
-		$categories=Category::all();
+        return view('blog.index')->withPosts($posts)->withCategories($categories)->withCatcnt($catcnt);
+    }
 
-		return view('blog.index')->withPosts($posts)->withCategories($categories)->withCatcnt($catcnt);
-	}
+    public function getSingle($slug)
+    {
 
-    public function getSingle($slug){
+        $post = Post::where('slug', '=', $slug)->first();
 
-    	$post=Post::where('slug','=',$slug)->first();
+        $posts = Post::where('published', '=', 1)->orderBy('id', 'desc')->limit(5)->get();
 
-    	$posts=Post::where('published','=',1)->orderBy('id','desc')->limit(5)->get();
+        $catcnt = [];
+        $posts_to_cnt = Post::where('published', 1)->get();
+        foreach ($posts_to_cnt as $post1) {
+            $catcnt[$post1->category_id] = Post::where('category_id', $post1->category_id)->count();
+        }
+        $categories = Category::all();
 
-		$catcnt=[];
-		$posts_to_cnt=Post::where('published',1)->get();
-		foreach ($posts_to_cnt as $post1) 
-		{
-			$catcnt[$post1->category_id]=Post::where('category_id',$post1->category_id)->count();	
-		}
-		$categories=Category::all();
-
-    	return view('blog.single')->withPost($post)->withPosts($posts)->withCategories($categories)->withCatcnt($catcnt);
+        return view('blog.single')->withPost($post)->withPosts($posts)->withCategories($categories)->withCatcnt($catcnt);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -8,15 +8,26 @@ class Post extends Model
 {
     public function Category()
     {
-    	return $this->belongsTo('App\Category');
+        return $this->belongsTo('App\Category');
     }
 
     public function Admin()
     {
-    	return $this->belongsTo('App\Admin');	
+        return $this->belongsTo('App\Admin');
     }
     public function comments()
     {
-    	return $this->hasMany('App\Comment');
+        return $this->hasMany('App\Comment');
+    }
+
+    /**
+     * Search using the search field on the title of the post
+     * @param $query the query builder
+     * @param $search the string to search on the title
+     * @return the query builder
+     */
+    public function scopeSearchTitle($query, $search)
+    {
+        return $query->where('title', 'like', '%' . $search . '%');
     }
 }
